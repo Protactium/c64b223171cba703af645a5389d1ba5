f76473dfc28f0ca9157db356aafbf87331f7b2aa
Protactium <145288275+Protactium@users.noreply.github.com>
2024-06-20 12:55:11 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,23 +7,26 @@ return {
         end)
     end,
     [4581966615] = function() -- anomic
-        for i,v in pairs(game:GetService("Workspace").Entities:GetChildren()) do
-            if string.find(v.Name:lower(), "print") then
-                local printer = v:FindFirstChildOfClass("Part")
-                local esp = Instance.new("BillboardGui")
-                local text = Instance.new("TextLabel")
-                text.Parent = esp
-                text.BackgroundTransparency = 1
-                text.BorderSizePixel = 0
-                text.Text = 'Printer - ' .. v.CounterPart:FindFirstChildOfClass("SurfaceGui"):FindFirstChildOfClass("TextLabel").Text
-                text.Size = UDim2.new(0,100,0,100)
-                text.Visible = true
-                text.TextColor3 = Color3.new(255,255,255)
-                esp.Name = "EspPOG"
-                esp.AlwaysOnTop = true
-                esp.Parent = printer
-                esp.Adornee = printer
-                esp.Size = UDim2.new(0,200,0,50)
+        while wait(5) do
+            for i,v in pairs(game:GetService("Workspace").Entities:GetChildren()) do
+                if string.find(v.Name:lower(), "print") then
+                    if v:FindFirstChild("HAHAHAHAAH") then v:Destroy() end
+                    local printer = v:FindFirstChildOfClass("Part")
+                    local esp = Instance.new("BillboardGui")
+                    local text = Instance.new("TextLabel")
+                    text.Parent = esp
+                    text.BackgroundTransparency = 1
+                    text.BorderSizePixel = 0
+                    text.Text = 'Printer - ' .. v.CounterPart:FindFirstChildOfClass("SurfaceGui"):FindFirstChildOfClass("TextLabel").Text
+                    text.Size = UDim2.new(0,100,0,100)
+                    text.Visible = true
+                    text.TextColor3 = Color3.new(255,255,255)
+                    esp.Name = "HAHAHAHAAH"
+                    esp.AlwaysOnTop = true
+                    esp.Parent = printer
+                    esp.Adornee = printer
+                    esp.Size = UDim2.new(0,200,0,50)
+                end
             end
         end
     end,
