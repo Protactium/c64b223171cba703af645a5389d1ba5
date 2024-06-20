60729deaed598f196a6492b4c8c88b4603af0178
Protactium <145288275+Protactium@users.noreply.github.com>
2024-06-20 19:35:01 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,28 +8,30 @@ return {
     end,
     [4581966615] = function() -- anomic
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Protactium/c64b223171cba703af645a5389d1ba5/main/anomic%20v.lua"))()
-        while wait(5) do
-            for i,v in pairs(game:GetService("Workspace").Entities:GetChildren()) do
-                if string.find(v.Name:lower(), "print") then
-                    if v:FindFirstChild("HAHAHAHAAH",true) then v:FindFirstChild("HAHAHAHAAH",true).Adornee = nil;v:FindFirstChild("HAHAHAHAAH",true):Destroy() end
-                    local printer = v:FindFirstChildOfClass("Part")
-                    local esp = Instance.new("BillboardGui")
-                    local text = Instance.new("TextLabel")
-                    text.Parent = esp
-                    text.BackgroundTransparency = 1
-                    text.BorderSizePixel = 0
-                    text.Text = 'Printer - ' .. v.CounterPart:FindFirstChildOfClass("SurfaceGui"):FindFirstChildOfClass("TextLabel").Text
-                    text.Size = UDim2.new(0,100,0,100)
-                    text.Visible = true
-                    text.TextColor3 = Color3.new(255,255,255)
-                    esp.Name = "HAHAHAHAAH"
-                    esp.AlwaysOnTop = true
-                    esp.Parent = printer
-                    esp.Adornee = printer
-                    esp.Size = UDim2.new(0,200,0,50)
+        task.spawn(function()
+            while wait(5) do
+                for i,v in pairs(game:GetService("Workspace").Entities:GetChildren()) do
+                    if string.find(v.Name:lower(), "print") then
+                        if v:FindFirstChild("HAHAHAHAAH",true) then v:FindFirstChild("HAHAHAHAAH",true).Adornee = nil;v:FindFirstChild("HAHAHAHAAH",true):Destroy() end
+                        local printer = v:FindFirstChildOfClass("Part")
+                        local esp = Instance.new("BillboardGui")
+                        local text = Instance.new("TextLabel")
+                        text.Parent = esp
+                        text.BackgroundTransparency = 1
+                        text.BorderSizePixel = 0
+                        text.Text = 'Printer - ' .. v.CounterPart:FindFirstChildOfClass("SurfaceGui"):FindFirstChildOfClass("TextLabel").Text
+                        text.Size = UDim2.new(0,100,0,100)
+                        text.Visible = true
+                        text.TextColor3 = Color3.new(255,255,255)
+                        esp.Name = "HAHAHAHAAH"
+                        esp.AlwaysOnTop = true
+                        esp.Parent = printer
+                        esp.Adornee = printer
+                        esp.Size = UDim2.new(0,200,0,50)
+                    end
                 end
             end
-        end
+        end)
     end,
     [4169490976] = function() -- mortem SHIT
         local url = "https://raw.githubusercontent.com/jehheb12u11aaaaz/1z1rewgy4wer-tgerjfuwqehuofhweuifhegryhr4tyhr/main/mmtfwerhfugbr3gfbfbi3refgiergfberigfer"
