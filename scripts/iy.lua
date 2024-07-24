38d5c52e1fcb92fc5bb3c589433ef812e5cc29f0
Protactium <145288275+Protactium@users.noreply.github.com>
2024-07-24 22:42:14 +0100

    Update iy.lua

--- scripts/iy.lua
+++ scripts/iy.lua
@@ -6,8 +6,8 @@ end
 pcall(function() getgenv().IY_LOADED = true end)
 
 local cloneref = cloneref or function(o) return o end
-COREGUI = cloneref(game:GetService("CoreGui"))
 Players = cloneref(game:GetService("Players"))
+COREGUI = cloneref(Players.LocalPlayer.PlayerGui)
 
 if not game:IsLoaded() then
     local notLoaded = Instance.new("Message")
@@ -161,26 +161,11 @@ function randomString()
 end
 
 PARENT = nil
-if get_hidden_gui or gethui then
-	local hiddenUI = get_hidden_gui or gethui
-	local Main = Instance.new("ScreenGui")
-	Main.Name = randomString()
-	Main.Parent = hiddenUI()
-	PARENT = Main
-elseif (not is_sirhurt_closure) and (syn and syn.protect_gui) then
-	local Main = Instance.new("ScreenGui")
-	Main.Name = randomString()
-	syn.protect_gui(Main)
-	Main.Parent = COREGUI
-	PARENT = Main
-elseif COREGUI:FindFirstChild('RobloxGui') then
-	PARENT = COREGUI.RobloxGui
-else
-	local Main = Instance.new("ScreenGui")
-	Main.Name = randomString()
-	Main.Parent = COREGUI
-	PARENT = Main
-end
+
+local Main = Instance.new("ScreenGui")
+Main.Name = randomString()
+Main.Parent = COREGUI
+PARENT = Main
 
 shade1 = {}
 shade2 = {}
